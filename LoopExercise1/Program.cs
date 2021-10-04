f9660293d597240a3c50ead67225179baf262862
coffie2000us <charlescoffie.cri@gmail.com>
2021-10-04 15:21:13 -0400

    initial commit

---
+++ LoopExercise1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace LoopExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[]{ 1, 2, 3};
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine($"{i}");
+            }
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++)
+
+                Console.WriteLine($"{i}");
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
